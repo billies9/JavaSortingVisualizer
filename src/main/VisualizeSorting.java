19678ebdd91850989b41f546ce85bfc3ef195cdd
billies9 <ninerpc9@comcast.net>
2020-05-10 22:13:06 -0700

    Update call to MergeSort/BubbleSort

--- src/main/VisualizeSorting.java
+++ src/main/VisualizeSorting.java
@@ -8,6 +8,7 @@ public class VisualizeSorting {
 
 	private static Thread thread;
 	
+	public static Integer[] toSort;
 	public static boolean isSorting = false;
 	public static int sleep = 10;
 	
@@ -22,14 +23,15 @@ public class VisualizeSorting {
 		
 		if (thread == null && !isSorting) {
 			isSorting = true;
+			toSort = new Integer[] {5, 4, 3, 2, 1};
 			
 			switch(type) {
 			case "Bubble":
-				Integer[] array = new Integer[] {5, 4, 3, 2, 1};
-				thread = new Thread(new BubbleSort(array));
+				thread = new Thread(new BubbleSort(toSort));
 				break;
-	//		case "Merge":
-	//			break
+			case "Merge":
+				thread = new Thread(new MergeSort());
+				break;
 	//		case "Insertion":
 	//			break
 			
