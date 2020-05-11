ead078ba84616b8e9b16ed1898a6324b6f490266
billies9 <ninerpc9@comcast.net>
2020-05-10 22:13:45 -0700

    Apply MergeSort recursions/first checks for sorted array

--- src/main/sorts/MergeSort.java
+++ src/main/sorts/MergeSort.java
@@ -4,25 +4,31 @@ import main.*;
 
 public class MergeSort implements Runnable {
 	
-	// Initialize sorting array
-	private Integer[] toSort;
-	// Initialize frame to view sorting method
-	// private VisualizerFrame frame;	
-	
-	public MergeSort(Integer[] toSort) {
-		// Assign to-be-sorted array to scope of class
-		this.toSort = toSort;
-		// Assign frame to scope of class - Drawing & painting 
-		// this.frame = frame;
-	}
-	
 	public void run() {
-		this.sort();
+		Integer[] toSort = VisualizeSorting.toSort;
+		sort(toSort);
 		VisualizeSorting.isSorting = false;
 	}
 	
-	public void sort() {
-		int mid = (toSort.length - 1) / 2;
-		System.out.println(Integer.toString(mid));
+	public void sort(Integer[] x) {
+		sort(x, 0, x.length-1);
+	}
+	
+	private void sort(Integer[] x, int first, int last) {
+		int mid, right, left;
+		int temp;
+		
+		if (first >= last) return;
+		
+		mid = (first + last) / 2;
+		
+		sort(x, first, mid);
+		sort(x, mid+1, last);
+		
+		left = first;
+		right = mid+1;
+		// Skip merge sorting, is possible
+		if (x[mid] >= x[right])
+			return;
 	}
 }
