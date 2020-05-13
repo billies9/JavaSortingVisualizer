8b12bfc9f966459603f6d804701005a71dea85d4
billies9 <ninerpc9@comcast.net>
2020-05-12 22:12:55 -0700

    InsertionSort implementation & comments

--- src/main/sorts/InsertionSort.java
+++ src/main/sorts/InsertionSort.java
@@ -1,10 +1,11 @@
 package main.sorts;
 
 import main.VisualizeSorting;
+import java.util.Arrays;
 
 public class InsertionSort implements Runnable {
 	/*
-	 * 1) Select first unsorted element
+	 * 1) Select first 'unsorted' element
 	 * 2) Swap elements to the right until sorted
 	 * 3) Advance counter to the right
 	 */
@@ -21,7 +22,31 @@ public class InsertionSort implements Runnable {
 	}
 	
 	private void sort() {
-		// Implemented through while loop and temp variable storage
+		// Iterate through all elements of Array
+		for (int i = 0; i < toSort.length; i++) {
+			// Assign first unsorted value to compare
+			int k = toSort[i];
+			// Assign break between unsorted / sorted arrays
+			int j = i - 1;
+			
+			/* Start with comparison between 1st and 2nd element (j>=0)
+			 * jth element must be greater than first unsorted value
+			 */
+			while (j >= 0 && toSort[j] > k) {
+				// Move element to right (toSort[j] > k)
+				toSort[j + 1] = toSort[j];
+				// Digress one step to left
+				j--;
+			}
+			// Move previously unsorted value to left - now 'sorted' up to iteration
+			toSort[j + 1] = k;
+			try {
+				Thread.sleep(VisualizeSorting.sleep);
+			}
+			catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		
 	}
 }
