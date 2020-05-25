fc233341b246e24d42e679c82f81793acf77d7b6
billies9 <ninerpc9@comcast.net>
2020-05-24 22:02:40 -0700

    Text entry listener

---
+++ src/main/TextListener.java
@@ -0,0 +1,30 @@
+package main;
+
+import javax.swing.JSlider;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+public class TextListener implements DocumentListener {
+
+	public TextListener(JTextField textField, JSlider slider) {
+		
+	}
+
+	@Override
+	public void insertUpdate(DocumentEvent event) {
+		
+		
+	}
+
+	@Override
+	public void removeUpdate(DocumentEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void changedUpdate(DocumentEvent event) {
+		
+	}
+}
